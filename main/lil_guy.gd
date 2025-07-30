f71fcb99466cd8abb808a7a778506cb825c73846
unknown <matthew.lowsley@students.plymouth.ac.uk>
2025-07-30 20:59:06 +0100

    Added Folder Structure

--- main/lil_guy.gd
+++ main/lil_guy.gd
@@ -5,19 +5,19 @@ var screen_size
 var start_pos
 
 func _ready():
-    screen_size = get_viewport_rect().size
-    hide()
+	screen_size = get_viewport_rect().size
+	hide()
 
 func _process(delta: float) -> void:
-    var velocity = Vector2.ZERO
-    velocity.x += 1
-    
-    position += velocity * delta * speed
-    
-    if (position.x > screen_size.x + 60):
-        position = start_pos
+	var velocity = Vector2.ZERO
+	velocity.x += 1
+	
+	position += velocity * delta * speed
+	
+	if (position.x > screen_size.x + 60):
+		position = start_pos
 
 func start(pos):
-    start_pos = pos
-    position = pos
-    show()
+	start_pos = pos
+	position = pos
+	show()
--- main/main.gd
+++ main/main.gd
@@ -1,4 +1,4 @@
 extends Node2D
 
 func _ready() -> void:
-    $LilGuy.start($StartPosition.position)
+	$LilGuy.start($StartPosition.position)
