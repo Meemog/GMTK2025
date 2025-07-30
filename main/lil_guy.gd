bc57e8e317143d75b8c93fcafa4f543b08a60016
Oscar <oscmog@gmail.com>
2025-07-30 18:04:28 +0100

    added loop

--- main/lil_guy.gd
+++ main/lil_guy.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @export var speed = 200
 var screen_size
+var start_pos
 
 func _ready():
     screen_size = get_viewport_rect().size
@@ -12,7 +13,11 @@ func _process(delta: float) -> void:
     velocity.x += 1
     
     position += velocity * delta * speed
+    
+    if (position.x > screen_size.x + 60):
+        position = start_pos
 
 func start(pos):
+    start_pos = pos
     position = pos
     show()
