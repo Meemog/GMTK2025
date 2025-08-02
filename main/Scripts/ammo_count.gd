da97e61470c2860e363ae19d0d15ac7fb2a3f4f0
unknown <matthew.lowsley@students.plymouth.ac.uk>
2025-08-02 01:56:45 +0100

    ammo counter now spins!

--- main/Scripts/ammo_count.gd
+++ main/Scripts/ammo_count.gd
@@ -4,6 +4,8 @@ var player: Player
 
 @onready var barrels: Array[Sprite2D] = [$Barrel/Chamber1, $Barrel/Chamber2, $Barrel/Chamber3, $Barrel/Chamber4, $Barrel/Chamber5, $Barrel/Chamber6]
 
+var tween : Tween
+
 func _ready() -> void:
     fill_bullets()
 
@@ -11,12 +13,21 @@ func _ready() -> void:
     player.reload.connect(_on_reload)
     
 func fill_bullets():
+    
     for i in range(6):
         barrels[i].texture = player.bullets[i].back_view_texture
+        
 
 func _on_fired():
     barrels[player.bullet_pointer].texture = null
-    $Barrel.rotation -= PI/3
+    
+    if tween:
+        tween.kill()
+    
+    tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+    tween.tween_property($Barrel, "rotation", $Barrel.rotation-(PI/3), player.firing_cooldown/2)
+    
+    #$Barrel.rotation -= PI/3
 
 func _on_reload():
     fill_bullets()
