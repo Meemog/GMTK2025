ffd1e8680de2fa36bb6a0961209346b333fdf3ce
unknown <matthew.lowsley@students.plymouth.ac.uk>
2025-08-02 13:17:10 +0100

    bullets can now apply effects to other bullets! Also add robin hood bullet!

--- main/Scripts/bullet.gd
+++ main/Scripts/bullet.gd
@@ -5,7 +5,11 @@ var damage = 10
 var knockback = 15
 var travel_vector
 var speed: int
+var piercing : bool = false
+var additional_status_effect : Array[StatusEffect] = []
 var has_hit: Array[Enemy] = []
+
+var status_effect_node_scene : PackedScene = preload("res://Scenes/status_effect_node.tscn")
 
 var data : Bullet 
 
@@ -20,12 +24,22 @@ func flying() -> void:
 
 func hit(target : Enemy) -> void:
     if target not in has_hit:
-        target.take_damage(data.damage)
+        target.take_damage(damage)
         target.process_knockback(knockback, travel_vector)
         has_hit.append(target)
+        apply_additional_status_effects(target)
         data.hit(target)
-        if not data.piercing:
+        if not piercing:
             queue_free()
+
+func apply_additional_status_effects(target : Enemy) -> void:
+    if len(additional_status_effect) == 0:
+        return
+    
+    for i in range(len(additional_status_effect)):
+        var new_status_effect : StatusEffectNode = status_effect_node_scene.instantiate()
+        new_status_effect.data = additional_status_effect[i]
+        target.add_child(new_status_effect)
 
 func _on_area_entered(area: Area2D) -> void:
     if area.collision_layer == 1:
